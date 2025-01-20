353e839756fb428738e6c7afc25e26061c30fff9
Ben van Loon <Bennie_vloon@outlook.com>
2025-01-21 00:36:14 +0100

    Update config.cpp

--- config.cpp
+++ config.cpp
@@ -30,7 +30,7 @@ class CfgSettings
 				main_addon = "LT_Template_Gear";
 				class Dependancies 
 				{
-					LT_Base[] = {"LT_Template_Base", {3,40,70}, "true"};
+					LT_Base[] = {"LT_Template_Base", {3,46,72}, "true"};
 					LT_Gear[] = {"LT_Template_Gear", {VERSION_AR}, "true"};
 				};
 			};
